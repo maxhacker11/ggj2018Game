1e3d0bad0e5b9ca980b8acae69e518d9a4546587
Dextozz <Keyboardmasherz@gmail.com>
2018-01-27 07:59:26 +0100

    Button,Tried Player death

---
+++ ProjectGGJ/Assets/Scripts/ButtonInteractable.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonInteractable : MonoBehaviour 
+{
+	public GameObject[] riseableObjects;
+	public GameObject[] movableObjects;
+
+	//Riseable objects
+	public float maxHeight;
+	public float speed;
+	float minHeight;
+	bool rise;
+
+	void Start()
+	{
+		try{
+			minHeight = riseableObjects [0].transform.position.y;
+		}
+		catch {
+			minHeight = 0.0f;
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			
+			StartCoroutine(ChangeHeight ());
+		}
+	}
+
+	IEnumerator ChangeHeight()
+	{
+		if (riseableObjects.Length == 0)
+			yield return null;
+
+		//For every single door
+		for (int i = 0; i < riseableObjects.Length; i++) {
+
+			if (riseableObjects [i].transform.position.y >= maxHeight)
+				rise = false;
+
+			if (riseableObjects [i].transform.position.y <= minHeight)
+				rise = true;
+
+			//Maximum height
+			while (rise && riseableObjects[i].transform.position.y <= maxHeight) {
+				riseableObjects [i].transform.position += Vector3.up * speed;
+				yield return new WaitForSeconds (0.01f);
+			}
+			while(!rise && riseableObjects[i].transform.position.y >= minHeight){
+				riseableObjects [i].transform.position -= Vector3.up * speed;
+				yield return new WaitForSeconds (0.01f);
+			}
+		}
+	}
+}
---
+++ ProjectGGJ/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDeath : MonoBehaviour {
+
+	GameObject deathPanel;
+	Image panelMat;
+	Color c;
+
+	public float speed;
+
+	void Start()
+	{
+		deathPanel = GameObject.Find ("DeathPanel");
+		panelMat = deathPanel.GetComponent<Image> ();
+
+		StartCoroutine (BlackTransition ());
+	}
+
+	IEnumerator BlackTransition()
+	{
+		for (float i = 0; i <= 1; i+=speed) {
+			Debug.Log (i);
+			c = panelMat.color;
+			c.a = i;
+			panelMat.material.color = c;
+			yield return new WaitForSeconds (0.05f);
+		}
+
+		Debug.Log (c.a);
+	}
+}
