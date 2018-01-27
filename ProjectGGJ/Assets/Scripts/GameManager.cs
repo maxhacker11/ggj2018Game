260e5565daaf56f44de907a7af12a04f01ad98ce
Dextozz <Keyboardmasherz@gmail.com>
2018-01-27 02:52:03 +0100

    Pusujem ti sve zivo haha

--- ProjectGGJ/Assets/Scripts/GameManager.cs
+++ ProjectGGJ/Assets/Scripts/GameManager.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	[SerializeField]
+	float playerSmallHealth = 100.0f;
+	[SerializeField]
+	float playerBigHealth = 100.0f;
+
+	[SerializeField]
+	float damage = 1.0f;
+
 	[HideInInspector]
 	public static int score;
 
 	GameObject infoPanel;
 	Text scoreText;
 
+	PlayeCamera cameraScript;
+
 	void Start()
 	{
+		cameraScript = GetComponent<PlayeCamera> ();
 		infoPanel = GameObject.Find ("InfoPanel");
 		scoreText = infoPanel.transform.Find ("ScoreText").GetComponent<Text> ();
 	}
 
 	void Update()
 	{
+		if (cameraScript.activeOne == true) {
+			playerSmallHealth -= damage * Time.deltaTime;
+		} else {
+			playerBigHealth -= damage * Time.deltaTime;
+		}
 		scoreText.text = score.ToString();
 	}
 }
